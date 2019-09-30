63608b09f968738c725abd203000cb69c789f74e
Jaromír Stanko <jaromir.stanko@aerobtec.com>
2019-09-30 15:05:47 +0200

    first commit

---
+++ Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_ll_rcc.h"
+#include "stm32f3xx_ll_bus.h"
+#include "stm32f3xx_ll_system.h"
+#include "stm32f3xx_ll_exti.h"
+#include "stm32f3xx_ll_cortex.h"
+#include "stm32f3xx_ll_utils.h"
+#include "stm32f3xx_ll_pwr.h"
+#include "stm32f3xx_ll_dma.h"
+#include "stm32f3xx_ll_gpio.h"
+
+#if defined(USE_FULL_ASSERT)
+#include "stm32_assert.h"
+#endif /* USE_FULL_ASSERT */
+
+//GPIOB peripheral base address
+#define	GPIOB_BASE_ADDR			(uint32_t)(0x48000400U)
+//MODER register
+#define	GPIOB_MODER_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x00U)
+//OTYPER register
+#define	GPIOB_OTYPER_REG		*(uint32_t *)(GPIOB_BASE_ADDR + 0x04U)
+//OSPEEDER register
+#define GPIOB_OSPEEDER_REG		*(uint32_t *)(GPIOB_BASE_ADDR + 0x08U)
+//PUPDR register
+#define GPIOB_PUPDR_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x0CU)
+//IDR register
+#define GPIOB_IDR_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x10U)
+//ODR register
+#define GPIOB_ODR_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x14U)
+//BSRR register
+#define GPIOB_BSRR_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x18U)
+//BRR register
+#define GPIOB_BRR_REG			*(uint32_t *)(GPIOB_BASE_ADDR + 0x28U)
+
+//RCC base address
+#define	RCC_BASE_ADDR			(uint32_t)(0x40021000U)
+//AHBEN register
+#define	RCC_AHBEN_REG			*((volatile uint32_t *) (uint32_t)(RCC_BASE_ADDR + 0x00000014U))
+
+#define LED_ON					*((volatile uint32_t *)((uint32_t)(0x48000400 + 0x18U))) |= (1 << 3)   	//GPIOB pin 3
+#define LED_OFF					*((volatile uint32_t *)((uint32_t)0x48000400 + 0x28U)) |= (1 << 3);		//GPIOB pin 3
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/main.c
@@ -0,0 +1,110 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+int main(void)
+{
+  /*Enables clock for GPIO port B*/
+  *((volatile uint32_t *) (uint32_t)(0x40021000 + 0x00000014U)) |= (uint32_t)(1 << 18);
+
+  /*GPIOB pin 3 and 6 setup*/
+  /*GPIO MODER register, reset*/
+  *((volatile uint32_t *)((uint32_t)0x48000400)) = 0;
+  //Set mode for pin 3 and 6
+  *((volatile uint32_t *)((uint32_t)0x48000400)) |= (uint32_t)(0x1 << 6);
+  *((volatile uint32_t *)((uint32_t)0x48000400)) |= (uint32_t)(0x0 << 0);
+
+  /*GPIO OTYPER register, reset*/
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x04U))) = 0;
+
+  /*GPIO OSPEEDR register, reset*/
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x08U))) = 0;
+  //Set Low speed for GPIOB pin 3
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x08U))) &= ~(0x3 << 6);
+
+  /*GPIO PUPDR register, reset*/
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) = 0;
+
+  while (1)
+  {
+	  //GPIO IDR, read input from pin 6
+	  if(*((volatile uint32_t *)((uint32_t)(0x48000400 + 0x10U))) & (1 << 6))
+	  {
+		  //GPIO BSRR register, set output pin 3
+		  LED_ON;
+		  //delay
+		  for(uint16_t i = 0; i < 0xFF00; i++){}
+		  //GPIO BRR, reset output pin 3
+		  LED_OFF;
+		  //delay
+		  for(uint16_t i = 0; i < 0xFF00; i++){}
+	  }
+	  else
+	  {
+		  //GPIO BSRR register, set output pin 3
+		  LED_ON;
+		  //delay
+		  for(uint32_t i = 0; i < 0xFFFFF; i++){}
+		  //GPIO BRR, reset output pin 3
+		  LED_OFF;
+		  //delay
+		  for(uint32_t i = 0; i < 0xFFF00; i++){}
+	  }
+  }
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
